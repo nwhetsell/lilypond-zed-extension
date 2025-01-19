f367996436fd3ea83d85e318c2ea6cfba08d8453
Nate Whetsell <nathan.whetsell@gmail.com>
2025-01-19 07:22:56 -0500

    Use standard Tree-sitter names for injections

--- languages/lilypond/injections.scm
+++ languages/lilypond/injections.scm
@@ -1,15 +1,9 @@
-; From https://tree-sitter.github.io/tree-sitter/syntax-highlighting#language-injection,
-; the standard capture for injections is @injection.content (not @content), and
-; the standard properties are injection.language and injection.combined (not
-; language and combined), but Zed doesn’t use standard Tree-sitter names. See
-; https://github.com/zed-industries/zed/issues/9656 and
-; https://github.com/zed-industries/zed/pull/9654 for more information. In
-; addition, Tree-sitter supports an injection.include-children property that
-; should be used here to support parsing LilyPond Scheme as a true injection,
-; but it appears that Zed doesn’t support that at all; searching the Zed
-; repository for include-children produces no results:
+; Tree-sitter supports an injection.include-children property that should be
+; used here to support parsing LilyPond Scheme as a true injection, but it
+; appears that Zed doesn’t support that. Searching the Zed repository for
+; include-children produces no results:
 ;   https://github.com/search?q=repo%3Azed-industries%2Fzed+include-children%22&type=code
 ((embedded_scheme
-    (embedded_scheme_text) @content)
-  (#set! language "lilypond_scheme")
-  (#set! combined))
+    (embedded_scheme_text) @injection.content)
+  (#set! injection.language "lilypond_scheme")
+  (#set! injection.combined))
--- languages/lilypond_scheme/injections.scm
+++ languages/lilypond_scheme/injections.scm
@@ -1,10 +1,4 @@
-; From https://tree-sitter.github.io/tree-sitter/syntax-highlighting#language-injection,
-; the standard capture for injections is @injection.content (not @content), and
-; the standard properties are injection.language and injection.combined (not
-; language and combined), but Zed doesn’t use standard Tree-sitter names. See
-; https://github.com/zed-industries/zed/issues/9656 and
-; https://github.com/zed-industries/zed/pull/9654 for more information.
 ((scheme_embedded_lilypond
-    (scheme_embedded_lilypond_text) @content)
-  (#set! language "lilypond")
-  (#set! combined))
+    (scheme_embedded_lilypond_text) @injection.content)
+  (#set! injection.language "lilypond")
+  (#set! injection.combined))
