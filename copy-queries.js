e21f3d92e1e1dd7b6bf77c6609410db5ca92311d
Nate Whetsell <nathan.whetsell@gmail.com>
2025-05-11 16:38:24 -0400

    Update copy-queries.js for multi-language grammar

--- copy-queries.js
+++ copy-queries.js
@@ -1,25 +1,14 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
-const toml = require('toml');
-
-const extension = toml.parse(fs.readFileSync('./extension.toml', 'utf8'));
 
 execSync(`
-  git clone ${extension.grammars.lilypond.repository} &&
-  cd tree-sitter-lilypond &&
-  git checkout ${extension.grammars.lilypond.commit}
+  git clone https://github.com/nwhetsell/tree-sitter-lilypond &&
+  cd tree-sitter-lilypond
 `);
 for (const suffix of ['', '-builtins']) {
   fs.cpSync(`tree-sitter-lilypond/queries/highlights${suffix}.scm`, `languages/lilypond/highlights${suffix}.scm`);
 }
-fs.rmSync('tree-sitter-lilypond', {recursive: true, force: true});
-
-execSync(`
-  git clone ${extension.grammars.lilypond_scheme.repository} &&
-  cd tree-sitter-lilypond-scheme &&
-  git checkout ${extension.grammars.lilypond_scheme.commit}
-`);
 for (const suffix of ['', '-builtins', '-lilypond-builtins']) {
-  fs.cpSync(`tree-sitter-lilypond-scheme/queries/highlights${suffix}.scm`, `languages/lilypond/highlights-scheme${suffix}.scm`);
+  fs.cpSync(`tree-sitter-lilypond-scheme/queries/highlights-scheme${suffix}.scm`, `languages/lilypond/highlights-scheme${suffix}.scm`);
 }
-fs.rmSync('tree-sitter-lilypond-scheme', {recursive: true, force: true});
+fs.rmSync('tree-sitter-lilypond', {recursive: true, force: true});
