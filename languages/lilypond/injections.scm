2d7ca26f00524cef515ee36322149d11c5551b4f
Nate Whetsell <nathan.whetsell@gmail.com>
2026-05-04 06:09:16 -0400

    Set injection.include-children

--- languages/lilypond/injections.scm
+++ languages/lilypond/injections.scm
@@ -1,9 +1,5 @@
-; Tree-sitter supports an injection.include-children property that should be
-; used here to support parsing LilyPond Scheme as a true injection, but it
-; appears that Zed doesn’t support that. Searching the Zed repository for
-; include-children produces no results:
-;   https://github.com/search?q=repo%3Azed-industries%2Fzed+include-children%22&type=code
 ((embedded_scheme
     (embedded_scheme_text) @injection.content)
   (#set! injection.language "lilypond_scheme")
-  (#set! injection.combined))
+  (#set! injection.combined)
+  (#set! injection.include-children))
