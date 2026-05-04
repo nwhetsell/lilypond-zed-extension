2ec9332f701502ec9caba1eaf859b4ea25cb01ff
Nate Whetsell <nathan.whetsell@gmail.com>
2026-05-04 06:07:00 -0400

    Prefer double quotes

--- copy-queries.js
+++ copy-queries.js
@@ -1,14 +1,14 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
+const { execSync } = require("child_process");
+const fs = require("fs");
 
 execSync(`
   git clone https://github.com/nwhetsell/tree-sitter-lilypond &&
   cd tree-sitter-lilypond
 `);
-for (const suffix of ['', '-builtins']) {
+for (const suffix of ["", "-builtins"]) {
   fs.cpSync(`tree-sitter-lilypond/queries/highlights${suffix}.scm`, `languages/lilypond/highlights${suffix}.scm`);
 }
-for (const suffix of ['', '-builtins', '-lilypond-builtins']) {
+for (const suffix of ["", "-builtins", "-lilypond-builtins"]) {
   fs.cpSync(`tree-sitter-lilypond/queries/highlights-scheme${suffix}.scm`, `languages/lilypond/highlights-scheme${suffix}.scm`);
 }
-fs.rmSync('tree-sitter-lilypond', {recursive: true, force: true});
+fs.rmSync("tree-sitter-lilypond", {recursive: true, force: true});
